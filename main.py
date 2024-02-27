e4c32710e9eae8649c98bade0553191a99cecf41
Anton Gasse <anton.gasse@gmx.de>
2024-02-27 12:01:27 +0100

    added docstrings

--- main.py
+++ main.py
@@ -7,15 +7,38 @@ from snake import Snake_Head
 from apple import Apple
 
 class Game():
-    def __init__(self):
-        
+    """
+    A class representing the game logic and mechanics.
+
+    Attributes:
+        screen (pygame.Surface): The surface where the game will be displayed.
+        pixels (int): The size of each game block.
+        clock (pygame.time.Clock): A clock object to control the game's frame rate.
+        font (pygame.font.Font): The font used for text rendering in the game.
+        large_font (pygame.font.Font): The font used for larger text rendering in the game.
+        gamestatus (str): The current status of the game ("pause" or "play").
+        game_map (Map): The map object containing game borders.
+        border_free_positions (list): A list of free positions on the game map not occupied by borders.
+        snake (Snake_Head): The snake head object.
+        next_moves (list): A list of directions for the snake to move next.
+        move_count (int): A counter for the number of moves made by the snake.
+        apple (Apple): The apple object.
+        last_move (str): The last direction the snake moved.
+        start_text (pygame.Surface): The text displayed when the game is paused, prompting the player to start.
+        score (int): The player's score.
+        score_text (pygame.Surface): The text displaying the player's score.
+    """
+    def __init__(self) -> None:
+        """
+        Initializes the Game object.
+        """
         pygame.init()
         self.screen = pygame.display.set_mode((900, 600), pygame.RESIZABLE)
         pygame.display.set_caption("Snake")
         self.pixels = 25
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font("utils/astroids-pixel-font.ttf", 30)
-        self.font_groß = pygame.font.Font("utils/astroids-pixel-font.ttf", 60)
+        self.large_font = pygame.font.Font("utils/astroids-pixel-font.ttf", 60)
         self.gamestatus = "pause"
         self.game_map = Map(self.screen)
         self.border_free_positions = self.get_border_free_positions()
@@ -26,10 +49,13 @@ class Game():
         self.last_move = "up"
         self.start_text = self.font.render("PRESS ENTER OR TAP TO START", False, "White")
         self.score = 0
-        self.score_text = self.font_groß.render(f"SCORE: {self.score}", False, [0, 155, 0])
+        self.score_text = self.large_font.render(f"SCORE: {self.score}", False, [0, 155, 0])
         
 
-    async def gameloop(self):
+    async def gameloop(self) -> None:
+        """
+        The main game loop.
+        """
         while True:
             while self.gamestatus == "pause":
                 self.pause_events()
@@ -58,7 +84,7 @@ class Game():
                     self.snake.add_tail()
                     self.spawn_apple()
                     self.score += 1
-                    self.score_text = self.font_groß.render(f"SCORE: {self.score}", False, [0, 155, 0])
+                    self.score_text = self.large_font.render(f"SCORE: {self.score}", False, [0, 155, 0])
                 if pygame.sprite.spritecollideany(self.snake, self.snake.get_tails()) and pygame.sprite.spritecollideany(self.snake, self.snake.get_tails()) != self.snake.get_tails().sprites()[0]:
                     self.gamestatus = "pause"                   
                 if pygame.sprite.spritecollideany(self.snake, self.game_map.get_borders()):
@@ -68,7 +94,11 @@ class Game():
                 self.clock.tick(60)
                 await asyncio.sleep(0)
         
-    def pause_events(self):
+
+    def pause_events(self) -> None:
+        """
+        Handles events while the game is paused.
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -81,7 +111,11 @@ class Game():
                 self.gamestatus = "play"
                 self.reset()
 
-    def play_events(self):
+
+    def play_events(self) -> None:
+        """
+        Handles events while the game is playing.
+        """
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -128,20 +162,31 @@ class Game():
                         self.next_moves.append("left")
                         self.last_move = "left"
         
-    def reset(self):
+
+    def reset(self) -> None:
+        """
+        Resets the game to its initial state.
+        """
         self.snake = Snake_Head(100, 400, self.screen)
         self.next_moves = []
         self.last_move = "up"
         self.move_count = 10
         self.score = 0
-        self.score_text = self.font_groß.render(f"SCORE: {self.score}", False, [0, 155, 0])
+        self.score_text = self.large_font.render(f"SCORE: {self.score}", False, [0, 155, 0])
     
-    def spawn_apple(self):
+
+    def spawn_apple(self) -> None:
         x, y = random.choice(self.get_possible_apple_positions())
         self.apple = Apple(x*self.pixels, y*self.pixels, self.screen)
         
 
     def get_possible_apple_positions(self) -> list[tuple[int, int]]:
+        """
+        Determines possible positions to spawn an apple.
+
+        Returns:
+            list[tuple[int, int]]: A list of possible positions.
+        """
         positions = []
         for x, y in self.border_free_positions:
             tmp_rect = pygame.Rect(self.pixels*x, self.pixels*y, self.pixels, self.pixels)
@@ -157,7 +202,14 @@ class Game():
         
         return positions
     
-    def get_border_free_positions(self):
+
+    def get_border_free_positions(self) -> list[tuple[int, int]]:
+        """
+        Determines the free positions on the game map not occupied by borders.
+
+        Returns:
+            list[tuple[int, int]]: A list of free positions.
+        """
         positions = []
         border_positions= [border.get_pos() for border in self.game_map.get_borders()]
         with open("utils/map.txt", "r") as m:
