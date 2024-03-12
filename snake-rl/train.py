b29784ffbcf45f9060b8ad0c1ec07a7ed3ec78a0
Anton Gasse <wy39otun@studserv.uni-leipzig.de>
2024-03-12 13:53:11 +0100

    fixed reinforcement learning and added trained model

--- snake-rl/train.py
+++ snake-rl/train.py
@@ -1,20 +1,14 @@
-from stable_baselines3 import DQN
+from stable_baselines3 import PPO
 from main_env import SnakeEnv
 
 env = SnakeEnv({})
 
 try:
-    model = DQN.load('model', env=env)
+    model = PPO.load('./models/model', env=env)
+    print("found model")
 except:
     print("found no model")
-    model = DQN("MlpPolicy", env, verbose=1)
+    model = PPO("MlpPolicy", env, verbose=1)
 
-model.learn(total_timesteps=1000000, log_interval=10)
-model.save('model')
-obs = env.reset()[0]
-done, truncated = False, False
-while not done and not truncated:
-    action, _states = model.predict(obs)
-    obs, rewards, done, truncated, info = env.step(int(action))
-    env.render()
-print(done, truncated)
+model.learn(total_timesteps=500000, log_interval=10)
+model.save('./models/model')
