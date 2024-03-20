1aa417cf082c2fee396e4d979dfd74be44747fe7
Anton Gasse <anton.gasse@gmx.de>
2024-03-20 16:37:55 +0100

    added web backend

---
+++ backend/app.py
@@ -0,0 +1,41 @@
+from flask import Flask, send_file, request
+from flask_cors import CORS, cross_origin
+from stable_baselines3 import PPO
+import json
+import os
+
+app = Flask(__name__)
+model = PPO.load(os.path.join("snake-rl", "models", "first_model.zip"))
+
+@app.route('/')
+def game():
+    with open(os.path.join("..", "frontend", "build", "web", "index.html"), "r", encoding='utf-8') as s:
+        return s.read()
+
+@app.route('/frontend.apk')
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def test_build():
+    return send_file(os.path.join("..", "frontend", "build", "web", "frontend.apk"))
+
+
+@app.route('/favicon.png')
+@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
+def favicon():
+    return send_file(os.path.join("..", "frontend", "build", "web", "favicon.png"))
+
+
+@app.route('/prediction', methods=['POST'])
+def prediction():
+    obs = request.get_json()['obs']
+    if model == None:
+        return "Currently no trained model available", 404
+    else:
+        prediction, _ = model.predict(obs)
+        prediction = {'prediction': int(prediction)}
+        return json.dumps(prediction, ensure_ascii=False)
+
+if __name__ == '__main__':
+    #context = ('./ssl_keys/cert.pem', './ssl_keys/key.pem')#certificate and key files
+    cors = CORS(app, resources={r'*': {"origins": '*'}})
+    
+    app.run(debug=True, ssl_context='adhoc', host="0.0.0.0", port=5001)
---
+++ frontend/webmodel.py
@@ -0,0 +1,90 @@
+import sys
+import platform
+from urllib.parse import urlencode
+import asyncio
+import json
+
+
+class Webmodel():
+    def __init__(self) -> None:
+        self.is_emscripten = sys.platform == "emscripten"
+        self.url = "https://127.0.0.1:5001/prediction"
+        POST = "POST"
+        if self.is_emscripten:
+            self._js_code = """
+window.Fetch = {}
+// generator functions for async fetch API
+// script is meant to be run at runtime in an emscripten environment
+// Fetch API allows data to be posted along with a POST request
+window.Fetch.POST = function * POST (url, data)
+{
+    // post info about the request
+    //console.log('POST: ' + url + 'Data: ' + data);
+    var request = new Request(url, {headers: {'Accept': 'application/json','Content-Type': 'application/json'},
+        method: 'POST',
+        body: data});
+    var content = 'undefined';
+    fetch(request)
+   .then(resp => resp.text())
+   .then((resp) => {
+        //console.log(resp);
+        content = resp;
+   })
+   .catch(err => {
+         // handle errors
+         console.log("An Error Occurred:")
+         console.log(err);
+    });
+    while(content == 'undefined'){
+        yield;
+    }
+    yield content;
+}
+// Only URL to be passed
+// when called from python code, use urllib.parse.urlencode to get the query string
+window.Fetch.GET = function * GET (url)
+{
+    console.log('GET: ' + url);
+    var request = new Request(url, { method: 'GET' })
+    var content = 'undefined';
+    fetch(request)
+   .then(resp => resp.text())
+   .then((resp) => {
+        console.log(resp);
+        content = resp;
+   })
+   .catch(err => {
+         // handle errors
+         console.log("An Error Occurred:");
+         console.log(err);
+    });
+    while(content == 'undefined'){
+        // generator
+        yield;
+    }
+
+    yield content;
+}
+            """
+            try:
+                platform.window.eval(self._js_code)
+            except AttributeError:
+                self.is_emscripten = False
+                print("mist")
+
+
+    async def predict(self, obs:tuple[int, int, int, int, int, int, int]) -> int:
+        data = {'obs': obs}
+        action = await self.post(data=data) 
+        return action
+    
+    async def post(self, data:dict=None):
+        if data is None:
+            data = {}
+        if self.is_emscripten:
+            await asyncio.sleep(0)
+            content = await platform.jsiter(platform.window.Fetch.POST(self.url, json.dumps(data, ensure_ascii=False)))
+            result = content
+            
+            return result
+        return ""
